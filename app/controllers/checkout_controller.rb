4d06297d78707d2ed4c8a61c7dd4c7a419cdc0f9
Waldo Uribe <uribe.fache@gmail.com>
2018-02-26 15:22:59 -0300

    first scaffolds

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,7 @@
+class CheckoutController < ApplicationController
+  def index
+  end
+
+  def register_transaction
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'landing/index'
+
+  root to: 'landing#index'
+  get 'checkout/index'
+  get 'checkout/register_transaction'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
